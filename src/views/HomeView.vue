4349152efc397f32c4f8d94bb269141316ebd888
=Restu Averian Putra <=restuaverianputra123@gmail.com>
2022-06-14 16:59:12 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <full-page ref="fullpage" :options="options">
+      <div class="section">First section ...</div>
+      <div class="section">Second section ...</div>
+      <div class="section">Third section ...</div>
+    </full-page>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        licenseKey: "YOUR_KEY_HEERE",
+        menu: "#menu",
+        anchors: ["page1", "page2", "page3"],
+        sectionsColor: ["#41b883", "#ff5f45", "#0798ec"],
+      },
+    };
+  },
+
+  methods: {
+    afterLoad() {
+      console.log("Emitted 'after load' event.");
+    },
+  },
+};
+</script>
